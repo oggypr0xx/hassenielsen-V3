81a317f7aace5907a077f4ee1ebf67e5b4b96990
oggypr0xx <oscarmeyer@Oscars-MacBook-Pro.local>
2013-11-14 18:34:36 +0100

    yes

---
+++ static/superbox.js
@@ -0,0 +1,82 @@
+/*
+	SuperBox v1.0.0
+	by Todd Motto: http://www.toddmotto.com
+	Latest version: https://github.com/toddmotto/superbox
+	
+	Copyright 2013 Todd Motto
+	Licensed under the MIT license
+	http://www.opensource.org/licenses/mit-license.php
+
+	SuperBox, the lightbox reimagined. Fully responsive HTML5 image galleries.
+*/
+;(function($) {
+		
+	$.fn.SuperBox = function(options) {
+		
+		var superbox      = $('<div class="superbox-show"></div>');
+		var superboximg   = $('<img src="" class="superbox-current-img">');
+		var superboxclose = $('<div class="superbox-close"></div>');
+		
+		superbox.append(superboximg).append(superboxclose);
+		
+		return this.each(function() {
+			
+		
+			
+			$('.superbox-list').click(function() {
+				
+				$('.superbox-show .content').remove();
+		
+				var currentimg = $(this).find('.superbox-img');
+				var imgData = currentimg.data('img');
+				superboximg.attr('src', imgData);
+				
+				var currentcontent = $(this).find('.superbox-content').html();
+				
+				if($('.superbox-current-img').css('opacity') == 0) {
+					$('.superbox-current-img').animate({opacity: 1});
+				}
+				
+				
+				
+				if ($(this).next().hasClass('superbox-show')) {
+					superbox.slideToggle();
+					
+					if($(this).hasClass('hover2')){ 
+						$(this).removeClass('hover2');
+					}else{$(this).addClass('hover2');}
+					
+					$('.superbox-show').append("<div class=content>"+currentcontent+"</div>");
+					
+					
+				} else {
+					superbox.insertAfter(this).css('display', 'block');
+					
+					if($(this).hasClass('hover2')){ 
+						$(this).removeClass('hover2');
+						}else{$(this).addClass('hover2');
+					}
+					
+					
+					
+					$('.superbox-show').append("<div class=content>"+currentcontent+"</div>");
+				
+				
+				}
+				
+				$('html, body').animate({
+					scrollTop:superbox.position().top - currentimg.width()
+				}, 'medium');
+			
+			});
+						
+			$('.superbox').on('click', '.superbox-close', function() {
+				$('.superbox-current-img').animate({opacity: 0}, 200, function() {
+					$('.superbox-show').slideUp();
+					$('.superbox-show .content').remove();
+				});
+			});
+			
+		});
+	};
+})(jQuery);
